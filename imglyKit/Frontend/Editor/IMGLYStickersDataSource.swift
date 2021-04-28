435fe06a5374ec8d7062635580ac65e1115f4e78
dtrimech-Ecocea <dtrimech@ecocea.com>
2021-04-28 17:43:50 +0200

    Update stickers

--- imglyKit/Frontend/Editor/IMGLYStickersDataSource.swift
+++ imglyKit/Frontend/Editor/IMGLYStickersDataSource.swift
@@ -17,23 +17,13 @@ open class IMGLYStickersDataSource: NSObject, IMGLYStickersDataSourceDelegate {
     
     override init() {
         let stickerFiles = [
-            "glasses_nerd",
-            "glasses_normal",
-            "glasses_shutter_green",
-            "glasses_shutter_yellow",
-            "glasses_sun",
-            "hat_cap",
-            "hat_party",
-            "hat_sherrif",
-            "hat_zylinder",
-            "heart",
-            "mustache_long",
-            "mustache1",
-            "mustache2",
-            "mustache3",
-            "pipe",
-            "snowflake",
-            "star"
+            "sticker_1",
+            "sticker_2",
+            "sticker_3",
+            "sticker_4",
+            "sticker_5",
+            "sticker_6",
+            "sticker_7"
         ]
         
         stickers = stickerFiles.map { (file: String) -> IMGLYSticker? in
